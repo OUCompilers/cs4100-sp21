724744d343743bc3ee2e748a1cc2e038c86a099e
Alex <ab667712@ohio.edu>
2021-01-30 00:45:49 -0500

    Remove unnecessary files

--- in-class/let_test/target/debug/deps/let_test-4d633911115140bf.d
+++
@@ -1,5 +0,0 @@
-/home/alex/Dropbox/cs4100/cs4100-sp21/in-class/let_test/target/debug/deps/let_test-4d633911115140bf: src/main.rs
-
-/home/alex/Dropbox/cs4100/cs4100-sp21/in-class/let_test/target/debug/deps/let_test-4d633911115140bf.d: src/main.rs
-
-src/main.rs:
--- in-class/let_test/target/debug/let_test.d
+++
@@ -1,1 +0,0 @@
-/home/alex/Dropbox/cs4100/cs4100-sp21/in-class/let_test/target/debug/let_test: /home/alex/Dropbox/cs4100/cs4100-sp21/in-class/let_test/src/main.rs
